bf003df568b7521089961d048773babfb5432108
Arseniy <andryuh.tech@zm.team>
2025-03-02 13:03:00 +0700

    a

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
     // import HeaderBlock from '@/components/HeaderBlock.vue';
     // import FooterBlock from '@/components/FooterBlock.vue';
+
+    // import { useRoute } from 'vue-router';
+
+    // const route = useRoute();
 </script>
 
 <template>
     <div class="app">
-        <!-- <header-block></header-block> -->
+        <!-- <header-block :key="route.path"></header-block> -->
         <router-view></router-view>
         <!-- <footer-block></footer-block> -->
     </div>
